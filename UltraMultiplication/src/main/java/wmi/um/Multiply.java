f544958f2f5fc08c23dfe839ded3940a61ada74a
Patryk Żywica <bikol@amu.edu.pl>
2019-11-04 16:32:48 +0100

    initial implementation

---
+++ UltraMultiplication/src/main/java/wmi/um/Multiply.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wmi.um;
+
+/**
+ *
+ * @author bikol
+ */
+public class Multiply {
+    public static String multiply(String a, String b){
+        int aa = Integer.parseInt(a);
+        int bb = Integer.parseInt(b);
+        if( aa<100 && bb<100){
+            return Integer.toString(aa+bb);
+        }
+        throw new IllegalArgumentException();
+    }
+}
---
+++ UltraMultiplication/src/test/java/wmi/um/MultiplyNGTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wmi.um;
+
+import java.util.Random;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author bikol
+ */
+public class MultiplyNGTest {
+
+    public MultiplyNGTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+
+    /**
+     * Test of multiply method, of class Multiply.
+     */
+    @Test
+    public void testMultiply() {
+        System.out.println("multiply");
+        String a = "1";
+        String b = "1";
+        String expResult = "2";
+        String result = Multiply.multiply(a, b);
+        assertEquals(result, expResult);
+    }
+
+    @Test
+    public void testMultiply2() {
+        System.out.println("multiply2");
+        Random r = new Random();
+
+        for (int i = 0; i < 100; i++) {
+            int aa = r.nextInt(100);
+            int bb = r.nextInt(100);
+            String a = Integer.toString(aa);
+            String b = Integer.toString(bb);
+            String expResult = Integer.toString(aa + bb);
+            String result = Multiply.multiply(a, b);
+            assertEquals(result, expResult);
+        }
+    }
+
+}
